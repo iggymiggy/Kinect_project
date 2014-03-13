c751ad10bab588871374d3799d487bf0f2050990
iggymiggy <mikonen28@gmail.com>
2014-03-13 20:29:04 +0200

    Pelin pohja

--- Kinect_project/App.xaml.cs
+++ Kinect_project/App.xaml.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿//------------------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
 
-namespace Kinect_project
+namespace ShapeGame
 {
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
--- Microsoft.Kinect.Toolkit/Properties/AssemblyInfo.cs
+++ Microsoft.Kinect.Toolkit/Properties/AssemblyInfo.cs
@@ -1,13 +1,15 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Kinect for Windows Developer Toolkit ShapeGame")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("Microsoft.Kinect.Toolkit")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,13 +34,8 @@ using System.Windows;
     // app, or any theme specific resource dictionaries)
 )]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion("1.8.0.0")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/kinect/2013", "Microsoft.Kinect.Toolkit")]
+[assembly: XmlnsPrefix("http://schemas.microsoft.com/kinect/2013", "k")]
